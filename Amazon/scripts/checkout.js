04fa9bd6c01849d705a9810ee9a5a652425b9fc4
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2024-11-27 19:57:53 -0500

    implemented await

--- Amazon/scripts/checkout.js
+++ Amazon/scripts/checkout.js
@@ -2,20 +2,11 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSymmary } from "./checkout/paymentSummary.js";
 import { loadProductsFetch } from "../data/products.js";
 
-/*
-new Promise((resolve) => {
-    loadProducts(()=> {
-        resolve();
-    });
-}).then(()=> {
-    
-})
-*/
-
-loadProductsFetch().then(() => {renderCheckOutPage()});
-
-function renderCheckOutPage() {
+async function renderCheckOutPage() {
+    await loadProductsFetch()
     renderPaymentSymmary();
     renderOrderSummary();
 
 }
+
+renderCheckOutPage()
