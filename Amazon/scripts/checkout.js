9d2717847ef5e55c210e990ac2c29643e084a574
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2024-11-27 19:09:55 -0500

    implemented promises

--- Amazon/scripts/checkout.js
+++ Amazon/scripts/checkout.js
@@ -2,10 +2,19 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSymmary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
+/*
+new Promise((resolve) => {
+    loadProducts(()=> {
+        resolve();
+    });
+}).then(()=> {
+    
+})
+*/
+
 loadProducts(renderCheckOutPage);
 
 function renderCheckOutPage() {
-
     renderPaymentSymmary();
     renderOrderSummary();
 
